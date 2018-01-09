f21dc4d94e0e076e61e56f8648f8ef36b43a92d6
David Gordon <heximhotep@gmail.com>
2018-01-09 15:24:29 -0800

    reorganized project -- all but current scripts and shaders moved to Old Scripts

--- Old Scripts/InstancerTestController.cs
+++ Old Scripts/InstancerTestController.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InstancerTestController : MonoBehaviour {
+public class InstancerTestController : MonoBehaviour
+{
     [SerializeField]
     Transform[] tforms;
     [SerializeField]
@@ -10,49 +11,58 @@ public class InstancerTestController : MonoBehaviour {
     [SerializeField]
     Material meshmat;
     Matrix4x4[] mforms;
-    TformData[] data;
+    Matrix4x4[] data;
     [SerializeField]
     ComputeShader shadyboi;
 
     ComputeBuffer bufferboi;
     ComputeBuffer outputboi;
 
-    struct TformData
+    // Use this for initialization
+    void Start()
     {
-        public Vector3 position;
-        public Vector3 direction;
-        public Vector3 up;
+        bufferboi = new ComputeBuffer(tforms.Length, sizeof(float) * 16);
+        outputboi = new ComputeBuffer(tforms.Length, sizeof(float) * 16);
+        data = new Matrix4x4[tforms.Length];
+        mforms = new Matrix4x4[tforms.Length];
+        foreach (Transform t in tforms)
+        {
+            GameObject cub = GameObject.CreatePrimitive(PrimitiveType.Cube);
+            cub.transform.SetParent(t, false);
+            cub.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+        }
     }
 
-	// Use this for initialization
-	void Start ()
+    void OnDestroy()
     {
-        bufferboi = new ComputeBuffer(tforms.Length, sizeof(float) * 9);
-        outputboi = new ComputeBuffer(tforms.Length, sizeof(float) * 16);
-        data = new TformData[tforms.Length];
-        mforms = new Matrix4x4[tforms.Length];
+        bufferboi.Release();
+        outputboi.Release();
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         for (int i = 0; i < data.Length; i++)
         {
             Transform form = tforms[i];
-            TformData datum = new TformData();
-            datum.position = form.position;
-            datum.direction = form.forward;
-            datum.up = form.up;
+            Matrix4x4 datum = new Matrix4x4(
+                    new Vector4(form.position.x, form.position.y, form.position.z),
+                    new Vector4(form.forward.x, form.forward.y, form.forward.z),
+                    new Vector4(),
+                    new Vector4()
+                );
+            
             data[i] = datum;
         }
         bufferboi.SetData(data);
+        bufferboi.GetData(data);
         shadyboi.SetInt("instanceCount", data.Length);
-        int kernelIdx = shadyboi.FindKernel("CSMain");
+        int kernelIdx = shadyboi.FindKernel("MakeInstanceMatrices");
         shadyboi.SetBuffer(kernelIdx, "instanceBuf", bufferboi);
         shadyboi.SetBuffer(kernelIdx, "outputBuf", outputboi);
-        int groupCount = data.Length / 10 + 1;
+        int groupCount = data.Length / 16 + 1;
         shadyboi.Dispatch(kernelIdx, groupCount, 1, 1);
         outputboi.GetData(mforms);
         Graphics.DrawMeshInstanced(mesh, 0, meshmat, mforms);
-	}
+    }
 }
